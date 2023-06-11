f11d313d933f95bc1af37ef35f6e3e824d6be836
gonChronoPer <gon.fer10@gmail.com>
2023-06-11 20:18:30 -0300

    Se agrego configuracion para habilitar Cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200', // Reemplaza esto con la URL de tu frontend
+    optionsSuccessStatus: 200,
+  };
 
   app.setGlobalPrefix('api');
 
@@ -13,6 +19,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.PORT || 3000);
 }
