c7a4fadb657927eaf2a397d7ec48c1608a87f760
gonChronoPer <gon.fer10@gmail.com>
2023-06-11 22:47:55 -0300

    Prueba CORS 5

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const cors = require('cors');
+
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
@@ -13,7 +15,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     })
   );
-  app.enableCors();
+  app.use(cors());
 
   await app.listen(process.env.PORT || 3000);
 }
